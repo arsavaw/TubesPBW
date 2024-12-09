a7feabb9d9e5430e03260d247fd2a71a0d29beaa
YohanLeonardo <yohanleonardo150@gmail.com>
2024-12-09 10:22:06 +0700

    delete files

--- tubesPBW/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "pbw4"
--- tubesPBW/src/main/resources/static/home.js
+++
@@ -1,9 +0,0 @@
-// Menangani klik pada tombol search
-document.getElementById("search-toggle").addEventListener("click", function(event) {
-    event.preventDefault();  // Mencegah aksi default
-    const searchForm = document.getElementById("search-form");
-    
-    // Toggle tampilkan form pencarian
-    searchForm.style.display = (searchForm.style.display === "block") ? "none" : "block";
-  });
-  
